952d6acb8ddd815efbb9e0e9537098464b000f80
Pallavi Langhe <1032202021@mitwpu.edu.in>
2023-12-17 16:38:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import {Suspense} from 'react';
+// import { BrowserRouter } from "react-router-dom";
+// import {Router} from './general/Router.jsx';
+// import {Loading} from './components/Loading.jsx';
+
+// export const PageWithHeader = ({children}) => (
+//   <div className="flex h-full flex-col">{children}</div>
+// );
+
+export const App = () => (
+  <div>
+   <p class="flex justify-center">Data Visualization Dashboard</p>
+  </div>
+  // <BrowserRouter>
+    
+  //   {/* <Suspense
+  //     fallback={
+  //       <PageWithHeader>
+  //         <Loading name="suspense"/>
+  //       </PageWithHeader>
+  //     }
+  //   >
+  //       <div className="h-full bg-indigo-50 p-4 lg:p-24">
+  //         <Router/>
+  //       </div>
+  //   </Suspense> */}
+  // </BrowserRouter>
+);
